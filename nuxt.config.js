45e0574b140ed6c9998972463a47c309837ee5dd
longhuicode <longhuicode@foxmail.com>
2022-05-27 15:08:32 +0800

    fix: optimize;

--- nuxt.config.js
+++ nuxt.config.js
@@ -123,8 +123,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    // For debug purpose
-    'html.minify': !abcConfig.isProd,
     extractCSS: true
   }
 }
